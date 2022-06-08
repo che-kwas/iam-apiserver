d303eedfd9066cca501211f9edf03c9fbbbcd117
Che Kwas <che.kwas@outlook.com>
2022-06-08 16:42:57 +0800

    feat: get token by secret

--- internal/pkg/token/token.go
+++ internal/pkg/token/token.go
@@ -8,10 +8,10 @@ import (
 
 func GenToken(kid string, secret string, expire time.Time) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"kid": kid,
 		"exp": expire.Unix(),
 		"iat": time.Now().Unix(),
 	})
 
+	token.Header["kid"] = kid
 	return token.SignedString([]byte(secret))
 }
